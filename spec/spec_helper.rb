7aff9bb95e35c23ba3681b4cd9c30f1213de3b45
Matt Outten <matt@outten.net>
2014-02-03 13:36:53 -0500

    Add focus flag config to spec tests
    To make it easier to focus on a single spec test, this commit adds the
    RSpec configuration options to allow the use of the focus tag to only
    run certain specs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,4 +10,7 @@ SafeYAML::OPTIONS[:default_mode] = :safe
 RSpec.configure do |config|
   config.platform = 'ubuntu'
   config.version  = '12.04'
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
+  config.treat_symbols_as_metadata_keys_with_true_values = true
 end
